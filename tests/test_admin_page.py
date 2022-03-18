e4ea4b49776fdd16f33f6d537090a09b1140f0ba
Gleb Gordeev <54059760+gleb-gordeev@users.noreply.github.com>
2022-03-19 00:56:11 +0300

    Add allure annotations

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -1,11 +1,15 @@
 from page_objects.AdminPage import AdminPage
 import helpers
+import allure
 
 
-url = "http://localhost/admin/"
+url = "http://192.168.1.105:80/admin/"
 
 
+@allure.title("Local opencart test #1")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_add_new_product(browser):
+    """this test fail without opencart"""
     r_device_name = helpers.random_string()
     browser.get(url)
     AdminPage(browser).login_with("user", "bitnami")
@@ -16,7 +20,10 @@ def test_add_new_product(browser):
     assert AdminPage(browser).search_element(r_device_name) == r_device_name
 
 
+@allure.title("Local opencart test #2")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_del_product(browser):
+    """this test fail without opencart"""
     browser.get(url)
     AdminPage(browser).login_with("user", "bitnami")
     AdminPage(browser).click_catalog()
@@ -27,21 +34,33 @@ def test_del_product(browser):
     assert new_device != deleted_device
 
 
+@allure.title("Local opencart test #3")
+@allure.severity(allure.severity_level.MINOR)
 def test_check_header_logo(browser):
+    """this test fail without opencart"""
     browser.get(url)
     AdminPage(browser).check_logo()
 
 
+@allure.title("Local opencart test #4")
+@allure.severity(allure.severity_level.MINOR)
 def test_check_help_text(browser):
+    """this test fail without opencart"""
     browser.get(url)
     AdminPage(browser).check_help_text()
 
 
+@allure.title("Local opencart test #5")
+@allure.severity(allure.severity_level.MINOR)
 def test_check_forgot_password(browser):
+    """this test fail without opencart"""
     browser.get(url)
     AdminPage(browser).check_forgot_password()
 
 
+@allure.title("Local opencart test #6")
+@allure.severity(allure.severity_level.MINOR)
 def test_check_login_button(browser):
+    """this test fail without opencart"""
     browser.get(url)
     AdminPage(browser).check_login_button()
